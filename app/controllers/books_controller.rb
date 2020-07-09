fa29e0598f1e545be0e31e7a2ca74f910bbc838f
futa1952 <futa1952@icloud.com>
2020-07-09 13:08:38 +0900

    [fonish]4 提出課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,23 @@
 class BooksController < ApplicationController
+
   def index
-    @books = Book.all
-    @book = Book.new
-  end
-
-  def show
-    @book = Book.find(params[:id])
-  end
-
-  def new
+    @books = Book.all.order(id: :ASC)
     @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all.order(id: :ASC)
+      render "index"
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -23,16 +25,23 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book.id)
+    else
+      render "edit"
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:destroy] = 'Book was successfully destoyed.'
+      redirect_to books_path
+    end
   end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
